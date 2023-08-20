0afa4584350ceb60777232f3487980ff5aec6cea
cs-308-2023 <amansharma110603@gmail.com>
2023-08-21 00:52:05 +0530

    Q8 commit done

---
+++ factorial.cpp
@@ -0,0 +1,13 @@
+#include "functions.h"
+
+int factorial(int n){
+    while(n<0) {
+        cout << "Please enter a positive number: ";
+        cin >> n;
+        cout << endl;
+    }
+    if(n!=1){
+	return(n * factorial(n-1));
+    }
+    else return 1;
+}
