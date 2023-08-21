b194306ad4d68c1cc5b049fe06e4d419afaa1ccb
mostlyaman <amansharma110603@gmail.com>
2023-08-21 15:22:44 +0530

    Q2: Change to printf statements

--- main.cpp
+++ main.cpp
@@ -1,9 +1,11 @@
 #include <iostream>
 #include "functions.h"
 using namespace std;
-int main(){
+int main()
+{
     print_hello();
-    cout<< endl;
-    cout<<"The factorial of 5 is "<< factorial(5)<< endl;
+    printf("\nThe factorial of 5 is ");
+    printf("%d\n", factorial(5));
+
     return 0;
 }
