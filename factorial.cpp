e76b2e54a013801d54d3392be98bcb48f0fac2b9
mostlyaman <amansharma110603@gmail.com>
2023-08-21 15:54:21 +0530

    Solve Merge Conflict

--- factorial.cpp
+++ factorial.cpp
@@ -1,11 +1,7 @@
 #include "functions.h"
 
 int factorial(int n){
-    if(n < 0) {
-        // Make changes in the master branch for merge conflict
-        return -1;
-    }
-        
+    if(n < 0) return -1;
     if(n!=1){
 	return(n * factorial(n-1));
     }
