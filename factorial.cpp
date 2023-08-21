1c09faaaa4812d389fce3398962d3d3311c6949f
mostlyaman <amansharma110603@gmail.com>
2023-08-21 15:05:17 +0530

    preparing for activity 3

--- factorial.cpp
+++ factorial.cpp
@@ -1,12 +1,6 @@
 #include "functions.h"
 
-// Make some more changes in the factorial.cpp file
 int factorial(int n){
-    while(n<0) {
-        cout << "Please enter a positive number: ";
-        cin >> n;
-        cout << endl;
-    }
     if(n!=1){
 	return(n * factorial(n-1));
     }
