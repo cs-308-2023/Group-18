2e66b2feca8db11c6bbe603f91e99900e7d10eaf
mostlyaman <amansharma110603@gmail.com>
2023-08-28 02:08:36 +0530

    Add rec function

--- gcd.cpp
+++ gcd.cpp
@@ -1,9 +1,6 @@
 int gcd(int x, int y) {
-    int div = x < y ? x : y;
-    
-    while(div--) {
-    	if(x % div == 0 && y % div == 0) return div;
+    if(y != 0) {
+    	return gcd(y, x % y);
     }
-    
-    return 1;
+    return x;
 }
