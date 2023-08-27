3b537764d48dbd7317d33a1fff9cec0a40ef6dca
mostlyaman <amansharma110603@gmail.com>
2023-08-28 02:26:21 +0530

    Also print result using recursive function

--- gcd.cpp
+++ gcd.cpp
@@ -1,6 +1,16 @@
 int gcd(int x, int y) {
+    int div = x < y ? x : y;
+    
+    while(div--) {
+    	if(x % div == 0 && y % div == 0) return div;
+    }
+    
+    return 1;
+}
+
+int gcd_rec(int x, int y) {
     if(y != 0) {
-    	return gcd(y, x % y);
+    	return gcd_rec(y, x % y);
     }
     return x;
 }
--- main.cpp
+++ main.cpp
@@ -4,8 +4,9 @@ using namespace std;
 int main()
 {
     int x = 56, y = 98;
-    
+
     cout << "The GCD of " << x << " and " << y << " is " << gcd(x, y) << ".\n";
+    cout << "The GCD of " << x << " and " << y << " using recursion is " << gcd_rec(x, y) << ".\n";
 
     return 0;
 }
